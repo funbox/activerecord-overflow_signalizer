da2510ba6d04308b1742f8c4270daec680a12c31
v.promzelev <v.promzelev@fun-box.ru>
2017-03-22 13:38:38 +0400

    basic functionality

--- lib/activerecord/overflow_signalizer.rb
+++ lib/activerecord/overflow_signalizer.rb
@@ -1,13 +1,65 @@
 require 'activerecord/overflow_signalizer/version'
+require 'active_record'
 
 module ActiveRecord
   class OverflowSignalizer
-    def initialize(logger:)
-      @logger = logger
+    class UnsupportedType < StandardError
+      attr_reader :type
+
+      def initialize(type = nil)
+        @type = type
+      end
     end
 
-    def analise!
+    DAY = 24 * 60 * 60
 
+    def initialize(logger: nil, models: nil, days_count: 60)
+      @logger = logger || ActiveRecord::Base.logger
+      @models = models || ActiveRecord::Base.descendants
+      @days_count = days_count
+    end
+
+    def analyse!
+      @models.group_by(&:table_name).each do |table, models|
+        model = models.first
+        pk = model.columns.select(&:primary).first
+        next if model.last.nil?
+        if (max_value(pk.sql_type) - model.last.id) / avg(model) <= @days_count
+          signalize(table, model.last.public_send(pk.name), max_value(pk.sql_type))
+        end
+      end
+    end
+
+    private
+
+    def avg(model)
+      yesterday = Time.now
+      week_records = (1..7).map do |t|
+        from = yesterday - DAY * t
+        to = from + DAY
+        model.where(created_at: from..to).count
+      end
+      week_records.reduce(:+) / week_records.keep_if { |v| v > 0 }.size
+    end
+
+    def max_value(type)
+      case type
+      when 'integer'
+        2_147_483_647
+      when 'bigint'
+        9_223_372_036_854_775_807
+      else
+        raise UnsupportedType, type
+      end
+    end
+
+    def signalize(table, current_value, max_value)
+      if current_value == max_value
+        msg = "Primary key in table #{table} overflowed! #{current_value} from #{max_value}"
+      else
+        msg = "Primary key in table #{table} will overflow soon! #{current_value} from #{max_value}"
+      end
+      @logger.warn(msg)
     end
   end
 end
--- spec/activerecord/overflow_signalizer_spec.rb
+++ spec/activerecord/overflow_signalizer_spec.rb
@@ -5,15 +5,81 @@ RSpec.describe ActiveRecord::OverflowSignalizer do
     expect(ActiveRecord::OverflowSignalizer::VERSION).not_to be nil
   end
 
-  describe '#analise!' do
+  describe '#analyse!' do
     context 'signalize to logger' do
       let!(:logger) { double(:logger, warn: true) }
 
-      subject { described_class.new(logger: logger) }
+      subject { described_class.new(logger: logger, models: [TestIntModel], days_count: 10) }
 
-      it 'doesn`t log anything' do
-        expect(logger).not_to receive(:warn)
-        subject.analise!
+      context 'empty table' do
+        it 'doesnt log anything' do
+          expect(logger).not_to receive(:warn)
+          subject.analyse!
+        end
+      end
+
+      context 'not empty table' do
+        let(:max_int) { 2_147_483_647 }
+        let(:day) { 24 * 60 * 60 }
+        let(:today) { Time.now }
+
+        context 'overflow far' do
+          before do
+            (1..7).each do |t|
+              TestIntModel.create!(created_at: today - day * t, updated_at: today - day * t)
+            end
+          end
+
+          after do
+            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH 1;})
+            TestIntModel.destroy_all
+          end
+
+          it 'doesnt log anything' do
+            expect(logger).not_to receive(:warn)
+            subject.analyse!
+          end
+        end
+
+        context 'overflow soon' do
+          before do
+            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH #{max_int - 16};})
+            (1..7).each do |t|
+              TestIntModel.create!(created_at: today - day * t, updated_at: today - day * t)
+            end
+          end
+
+          after do
+            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH 1;})
+            TestIntModel.destroy_all
+          end
+
+          it 'log about owerflow' do
+            expect(logger).to receive(:warn)
+              .with("Primary key in table #{TestIntModel.table_name} will overflow soon! #{TestIntModel.last.id} from #{max_int}")
+            subject.analyse!
+          end
+        end
+
+        context 'overflowed' do
+          before do
+            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH #{max_int - 6};})
+            (1..7).each do |t|
+              TestIntModel.create!(created_at: today - day * t, updated_at: today - day * t)
+            end
+          end
+
+          after do
+            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH 1;})
+            TestIntModel.destroy_all
+          end
+
+          it 'log about owerflow' do
+            expect(logger).to receive(:warn)
+              .with("Primary key in table #{TestIntModel.table_name} overflowed! #{TestIntModel.last.id} from #{max_int}")
+            subject.analyse!
+          end
+        end
       end
     end
   end
