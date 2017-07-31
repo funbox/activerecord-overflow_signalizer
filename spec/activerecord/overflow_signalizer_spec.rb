47c38e70625accfae0da8312a1e669cd6481ee0f
v.promzelev <v.promzelev@fun-box.ru>
2017-07-31 16:42:37 +0400

    do not dublicate tast for analyse! and analyse

--- spec/activerecord/overflow_signalizer_spec.rb
+++ spec/activerecord/overflow_signalizer_spec.rb
@@ -13,7 +13,8 @@ RSpec.describe ActiveRecord::OverflowSignalizer do
 
   describe '#analyse!' do
     context 'raise exception' do
-      subject { described_class.new(models: [TestIntModel], days_count: 10) }
+      let(:logger) { Logger.new('/dev/null') }
+      subject { described_class.new(logger: logger, models: [TestIntModel], days_count: 10) }
 
       context 'empty table' do
         it { expect { subject.analyse! }.not_to raise_error }
@@ -21,7 +22,6 @@ RSpec.describe ActiveRecord::OverflowSignalizer do
 
       context 'unsupported type of primary key' do
         let(:today) { Time.now }
-        let(:logger) { Logger.new('/dev/null') }
         before do
           (1..7).each do |t|
             record = TestStringModel.new(created_at: today - day * t, updated_at: today - day * t)
@@ -83,158 +83,6 @@ RSpec.describe ActiveRecord::OverflowSignalizer do
           end
 
           it { expect { subject.analyse! }.to raise_error(described_class::Overflow) }
-        end
-      end
-    end
-  end
-
-  describe '#analyse' do
-    context 'signalize to logger' do
-      let!(:logger) { Logger.new('/dev/null') }
-
-      subject { described_class.new(logger: logger, models: [TestIntModel], days_count: 10) }
-
-      context 'empty table' do
-        it 'doesnt log to warn' do
-          expect(logger).not_to receive(:warn)
-          subject.analyse
-        end
-      end
-
-      context 'not empty table' do
-        let(:today) { Time.now }
-
-        context 'overflow far' do
-          before do
-            (1..7).each do |t|
-              TestIntModel.create!(created_at: today - day * t, updated_at: today - day * t)
-            end
-          end
-
-          after do
-            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH 1;})
-            TestIntModel.destroy_all
-          end
-
-          it 'doesnt log to warn' do
-            expect(logger).not_to receive(:warn)
-            subject.analyse
-          end
-        end
-
-        context 'overflow soon' do
-          before do
-            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH #{max_int - 16};})
-            (1..7).each do |t|
-              TestIntModel.create!(created_at: today - day * t, updated_at: today - day * t)
-            end
-          end
-
-          after do
-            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH 1;})
-            TestIntModel.destroy_all
-          end
-
-          it 'log about owerflow' do
-            expect(logger).to receive(:warn)
-              .with("Owerflowed tables: #{[]}. Overflow soon tables: #{[TestIntModel.table_name]}")
-            subject.analyse
-          end
-        end
-
-        context 'overflowed' do
-          before do
-            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH #{max_int - 6};})
-            (1..7).each do |t|
-              TestIntModel.create!(created_at: today - day * t, updated_at: today - day * t)
-            end
-          end
-
-          after do
-            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH 1;})
-            TestIntModel.destroy_all
-          end
-
-          it 'log about owerflow' do
-            expect(logger).to receive(:warn)
-              .with("Owerflowed tables: #{[TestIntModel.table_name]}. Overflow soon tables: #{[]}")
-            subject.analyse
-          end
-        end
-      end
-    end
-
-    context 'custom signalizer' do
-      let!(:signalizer) { double(:signalizer, signalize: true) }
-
-      subject { described_class.new(signalizer: signalizer, models: [TestIntModel], days_count: 10) }
-
-      context 'empty table' do
-        it 'doesnt log to warn' do
-          expect(signalizer).not_to receive(:signalize)
-          subject.analyse
-        end
-      end
-
-      context 'not empty table' do
-        let(:today) { Time.now }
-
-        context 'overflow far' do
-          before do
-            (1..7).each do |t|
-              TestIntModel.create!(created_at: today - day * t, updated_at: today - day * t)
-            end
-          end
-
-          after do
-            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH 1;})
-            TestIntModel.destroy_all
-          end
-
-          it 'doesnt log to warn' do
-            expect(signalizer).not_to receive(:signalize)
-            subject.analyse
-          end
-        end
-
-        context 'overflow soon' do
-          before do
-            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH #{max_int - 16};})
-            (1..7).each do |t|
-              TestIntModel.create!(created_at: today - day * t, updated_at: today - day * t)
-            end
-          end
-
-          after do
-            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH 1;})
-            TestIntModel.destroy_all
-          end
-
-          it 'log about owerflow' do
-            expect(signalizer).to receive(:signalize)
-              .with("Owerflowed tables: #{[]}. Overflow soon tables: #{[TestIntModel.table_name]}")
-            subject.analyse
-          end
-        end
-
-        context 'overflowed' do
-          before do
-            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH #{max_int - 6};})
-            (1..7).each do |t|
-              TestIntModel.create!(created_at: today - day * t, updated_at: today - day * t)
-            end
-          end
-
-          after do
-            TestIntModel.connection.execute(%Q{ALTER SEQUENCE "int_test_id_seq" RESTART WITH 1;})
-            TestIntModel.destroy_all
-          end
-
-          it 'log about owerflow' do
-            expect(signalizer).to receive(:signalize)
-              .with("Owerflowed tables: #{[TestIntModel.table_name]}. Overflow soon tables: #{[]}")
-            subject.analyse
-          end
         end
       end
     end
